b501ddc5dd368c838e3582d1c0150824413ac02e
decchi <idemasahiro@hotmail.co.jp>
2017-10-19 05:04:53 +0900

    first commit

---
+++ Assets/HoloBrainStroming/Scripts/InputStickyText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class InputStickyText : MonoBehaviour ,IFocusable{
+
+    public GameObject XPic;
+    public void OnFocusEnter()
+    {
+        //MakeStickyManager.Instance.m_DictationRecognizer.Start();
+        XPic.SetActive(false);
+    }
+
+    public void OnFocusExit()
+    {
+        //MakeStickyManager.Instance.m_DictationRecognizer.Stop();
+        XPic.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/HoloBrainStroming/Scripts/MakeStickyManager.cs
@@ -0,0 +1,44 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System;
+using UnityEngine.UI;
+
+public class MakeStickyManager : Singleton<MakeStickyManager>
+{
+    public DictationRecognizer m_DictationRecognizer;
+    public GameObject HoloSticky;
+    public GameObject FukidashiUI;
+    private List<GameObject> stickyList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        m_DictationRecognizer = new DictationRecognizer();
+        m_DictationRecognizer.InitialSilenceTimeoutSeconds = 30f;
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            
+            MakeSticky(text);
+        };
+        m_DictationRecognizer.Start();
+    }
+
+    public void MakeSticky(string text)
+    {
+        GameObject Sticky = GameObject.Instantiate(HoloSticky);
+        Sticky.transform.transform.Find("Text").GetComponent<Text>().text = text;
+        stickyList.Add(Sticky);
+        Sticky.transform.parent = FukidashiUI.transform;
+        Sticky.transform.localPosition = new Vector3(0f, -8f - stickyList.Count * 10, 0f);
+        Sticky.transform.localScale = new Vector3(0.148f, 0.148f, 0.148f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
